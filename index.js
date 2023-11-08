be766dc69c9e45b60849a0e4d54510d918c5ed4b
Hoxton <hoxton275373327@gmail.com>
2023-11-08 19:52:20 +0800

    修改佈局

---
+++ index.js
@@ -0,0 +1,76 @@
+function clearText() {
+    document.getElementById("inputText").value = "";
+}
+function formatText() {
+
+    const inputText = document.getElementById("inputText").value;
+    if (inputText == "") {
+        return;
+    }
+    const lines = inputText.split('\n');
+    let formattedText = "";
+
+    for (let i = 0; i < lines.length; i++) {
+        const trimmedLine = lines[i].trim();
+        if (i % 2 === 0) {
+            formattedText += trimmedLine;
+        } else {
+            formattedText += trimmedLine + "\n";
+        }
+
+        if (i % 2 === 0 && i < lines.length - 2) {
+            formattedText += " ";
+        }
+    }
+
+    // Remove a space after the colon on each line
+    formattedText = formattedText.replace(/:\s/g, ':');
+
+    document.getElementById("outputText").textContent = formattedText;
+
+
+
+    // Create a new history item
+    const historyDiv = document.getElementById("history");
+    const historyItem = document.createElement("div");
+    historyItem.className = "history-item";
+
+    // Create a card for the history item content
+    const formattedHistory = document.createElement("textarea");
+    formattedHistory.textContent = formattedText;
+    formattedHistory.className = "card";
+
+    // Create a delete button for the history item
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete-button btn btn-danger btn-sm";
+    deleteButton.addEventListener("click", function () {
+        historyDiv.removeChild(historyItem);
+    });
+
+    // Append the formatted text and delete button to the history item
+    historyItem.appendChild(formattedHistory);
+    historyItem.appendChild(deleteButton);
+
+    // Append the history item to the history section
+    historyDiv.appendChild(historyItem);
+}
+
+function copyToClipboard() {
+    const outputText = document.getElementById("outputText");
+    const textArea = document.createElement("textarea");
+    textArea.value = outputText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+
+    const copySuccessMessage = document.getElementById("copySuccessMessage");
+    copySuccessMessage.style.opacity = 1;
+
+    // 設置計時器，在2秒後淡出訊息
+    setTimeout(() => {
+        copySuccessMessage.style.opacity = 0;
+    }, 2000);
+
+}
