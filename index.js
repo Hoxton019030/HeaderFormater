97d68b809dd0b115b311911792fc9115e3913e7a
Hoxton <hoxton019030@gmail.com>
2023-11-09 00:35:35 +0800

    調整

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function clearText() {
     document.getElementById("inputText").value = "";
 }
+var dataArray = [];
+
 function formatText() {
 
     const inputText = document.getElementById("inputText").value;
@@ -25,35 +27,42 @@ function formatText() {
 
     // Remove a space after the colon on each line
     formattedText = formattedText.replace(/:\s/g, ':');
-
     document.getElementById("outputText").textContent = formattedText;
-
-
-
+    if (dataArray.length >= 10) {
+        dataArray.pop()
+    }
+    dataArray.unshift(formattedText);
+    const historyEntryDiv = document.getElementsByClassName("history-entry");
+    for (let i = 0; i < historyEntryDiv.length; i++) {
+        historyEntryDiv[i].innerHTML = ""; // 清空每个元素的内容
+    }
     // Create a new history item
-    const historyDiv = document.getElementsByClassName("history");
-    const historyItem = document.createElement("div");
-    historyItem.className = "history-item";
+    for (var i = 0; i < dataArray.length; i++) {
+        const historyDiv = document.getElementsByClassName("history");
+        const historyItem = document.createElement("div");
+        historyItem.className = "history-entry";
 
-    // Create a card for the history item content
-    const formattedHistory = document.createElement("textarea");
-    formattedHistory.textContent = formattedText;
-    formattedHistory.className = "card";
+        // Create a card for the history item content
+        const formattedHistory = document.createElement("textarea");
+        formattedHistory.textContent = dataArray[i];
+        formattedHistory.className = "card";
 
-    // Create a delete button for the history item
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delete-button btn btn-danger btn-sm";
-    deleteButton.addEventListener("click", function () {
-        historyDiv[0].removeChild(historyItem);
-    });
+        // Create a delete button for the history item
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "delete-button btn btn-danger btn-sm";
+        deleteButton.addEventListener("click", function () {
+            historyDiv[0].removeChild(historyItem);
+        });
 
-    // Append the formatted text and delete button to the history item
-    historyItem.appendChild(formattedHistory);
-    historyItem.appendChild(deleteButton);
+        // Append the formatted text and delete button to the history item
+        historyItem.appendChild(formattedHistory);
+        historyItem.appendChild(deleteButton);
 
-    // Append the history item to the history section
-    historyDiv[0].appendChild(historyItem);
+        // Append the history item to the history section
+        historyDiv[0].appendChild(historyItem);
+    }
+
 }
 
 function copyToClipboard() {
